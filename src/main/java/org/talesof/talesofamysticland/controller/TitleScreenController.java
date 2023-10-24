8e294abb12f2fce0dc71983ef2e8c3d9eb9e009f
Mateus <mateusc.ramos@outlook.com>
2023-10-24 00:39:17 -0300

    title screen fxml and controller implemented

---
+++ src/main/java/org/talesof/talesofamysticland/controller/TitleScreenController.java
@@ -0,0 +1,26 @@
+package org.talesof.talesofamysticland.controller;
+
+import javafx.fxml.FXML;
+
+public class TitleScreenController {
+    
+    @FXML 
+    public void onClickImgOpenConfigurations() {
+        // TODO
+    }
+
+    @FXML
+    public void onActionBtnPlay() {
+        // TODO
+    }
+
+    @FXML
+    public void onActionBtnExit() {
+        // TODO
+    }
+
+    @FXML
+    public void onActionHplRedirectToRegisterPlayer() {
+        // TODO
+    }
+}
