2c6a272e9574a08ba151f2f0ac9bec9a5d1493c1
Mateus <mateusc.ramos@outlook.com>
2023-10-24 00:38:54 -0300

    login fxml and controller implemented

---
+++ src/main/java/org/talesof/talesofamysticland/controller/LoginController.java
@@ -0,0 +1,38 @@
+package org.talesof.talesofamysticland.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class LoginController {
+
+    @FXML
+    private TextField txfUsernameOrEmail;
+
+    @FXML
+    private PasswordField pwfPassword;
+    
+    @FXML
+    public void onClickImgOpenConfigurations() {
+        // TODO
+    }
+
+    @FXML
+    public void onActionHplRedirectToChangePassword() {
+        // TODO
+    }
+
+    @FXML
+    public void onActionBtnLogin() {
+        // TODO
+    }
+
+    @FXML
+    public void onActionHplRedirectToRegisterPlayer() {
+        // TODO
+    }
+
+    @FXML
+    public void onActionBtnBack() {
+        // TODO
+    }
+}
