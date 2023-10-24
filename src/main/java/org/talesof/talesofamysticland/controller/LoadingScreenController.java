27ca61e258742fb1a4e7cc8ba160b0c332e845ba
Mateus <mateusc.ramos@outlook.com>
2023-10-24 00:39:41 -0300

    loading screen fxml and controller implemented

---
+++ src/main/java/org/talesof/talesofamysticland/controller/LoadingScreenController.java
@@ -0,0 +1,10 @@
+package org.talesof.talesofamysticland.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+
+public class LoadingScreenController {
+    
+    @FXML
+    private ProgressBar pgbLoading;
+}
