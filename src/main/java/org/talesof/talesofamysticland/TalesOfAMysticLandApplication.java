d526fb1e33d012c9a3eaded6a037c768ecd14c01
Mateus <mateusc.ramos@outlook.com>
2023-10-24 00:40:05 -0300

    change password fxml and controller implemented

--- src/main/java/org/talesof/talesofamysticland/TalesOfAMysticLandApplication.java
+++ src/main/java/org/talesof/talesofamysticland/TalesOfAMysticLandApplication.java
@@ -12,7 +12,7 @@ public class TalesOfAMysticLandApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(TalesOfAMysticLandApplication.class.getResource("view/register-player.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TalesOfAMysticLandApplication.class.getResource("view/change-password3-new-password.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.setTitle("Tales of a Mystic Land");
---
+++ src/main/java/org/talesof/talesofamysticland/controller/ChangePasswordController.java
@@ -0,0 +1,45 @@
+package org.talesof.talesofamysticland.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class ChangePasswordController {
+
+    @FXML
+    private TextField txfEmail;
+
+    @FXML
+    private TextField txfVerificationToken;
+
+    @FXML
+    private PasswordField pwfNewPassword;
+
+    @FXML
+    private PasswordField pwfConfirmedNewPassword;
+
+    @FXML
+    public void onClickImgOpenConfigurations() {
+        // TODO
+    }
+
+    @FXML
+    public void onActionBtnSendToken() {
+        // TODO
+    }
+
+    @FXML
+    public void onActionBtnValidateToken() {
+        // TODO
+    }
+
+    @FXML
+    public void onActionBtnSetNewPassword() {
+        // TODO
+    }
+
+    @FXML
+    public void onActionBtnBack() {
+        // TODO
+    }
+
+}
