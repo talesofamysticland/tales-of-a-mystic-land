32b07cfa3af6f80544e29588d483741e357ac421
Mateus <mateusc.ramos@outlook.com>
2023-10-30 23:19:31 -0300

    save selection view

--- src/main/java/org/talesof/talesofamysticland/TalesOfAMysticLandApplication.java
+++ src/main/java/org/talesof/talesofamysticland/TalesOfAMysticLandApplication.java
@@ -12,7 +12,7 @@ public class TalesOfAMysticLandApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(TalesOfAMysticLandApplication.class.getResource("view/login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TalesOfAMysticLandApplication.class.getResource("view/save-selection.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.setTitle("Tales of a Mystic Land");
---
+++ src/main/java/org/talesof/talesofamysticland/controller/SaveSelectionController.java
@@ -0,0 +1,44 @@
+package org.talesof.talesofamysticland.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class SaveSelectionController {
+
+    @FXML
+    private Label lblCharacterName1;
+
+    @FXML
+    private Label lblPlayTime1;
+
+    @FXML
+    private Label lblLastSaved1;
+
+    @FXML
+    private Label lblCharacterName2;
+
+    @FXML
+    private Label lblPlayTime2;
+
+    @FXML
+    private Label lblLastSaved2;
+
+    @FXML
+    private Label lblCharacterName3;
+
+    @FXML
+    private Label lblPlayTime3;
+
+    @FXML
+    private Label lblLastSaved3;
+    
+    @FXML 
+    public void onClickImgOpenConfigurations() {
+        // TODO
+    }
+
+    @FXML
+    public void onActionBtnBack() {
+        // TODO
+    }
+}
