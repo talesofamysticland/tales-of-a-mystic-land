b883d8aa685e13c072a618971cd36ac0949b3f5e
Mateus <mateusc.ramos@outlook.com>
2023-10-31 23:09:01 -0300

    border pane and css in character creation view

--- src/main/java/org/talesof/talesofamysticland/TalesOfAMysticLandApplication.java
+++ src/main/java/org/talesof/talesofamysticland/TalesOfAMysticLandApplication.java
@@ -9,14 +9,25 @@ import javafx.stage.Stage;
 
 public class TalesOfAMysticLandApplication extends Application {
 
+    final int originalTileSize = 16;  // 16x16
+    final int scale = 5;
+
+    public final int tileSize = originalTileSize * scale;  // 48x48
+    public final int maxScreenCol = 16; //5x4, 16x9, 21x9
+    public final int maxScreenRow = 9; //5x4, 16x9, 21x9
+    public final int screenWidth = tileSize * maxScreenCol;  // 960px
+    public final int screenHeight = tileSize * maxScreenRow;  // 576px
+
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(TalesOfAMysticLandApplication.class.getResource("view/save-selection.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TalesOfAMysticLandApplication.class.getResource("view/character-creation.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.setTitle("Tales of a Mystic Land");
-        stage.sizeToScene();
+        stage.setResizable(true);
+        stage.setWidth(screenWidth);
+        stage.setHeight(screenHeight);
         stage.show();
     }
 
---
+++ src/main/java/org/talesof/talesofamysticland/controller/CharacterCreationController.java
@@ -0,0 +1,16 @@
+package org.talesof.talesofamysticland.controller;
+
+import javafx.fxml.FXML;
+
+public class CharacterCreationController {
+    
+     @FXML 
+    public void onClickImgOpenConfigurations() {
+        // TODO
+    }
+
+    @FXML
+    public void onActionBtnBack() {
+        // TODO
+    }
+}
