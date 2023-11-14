00d9714ce1aab5d78d50eb4dd8d5f539dcd50551
mat-afk <mateusc.ramos@outlook.com>
2023-11-13 22:59:51 -0300

    feat: class selector & more fonts

--- src/main/java/org/talesof/talesofamysticland/controller/CharacterCreationController.java
+++ src/main/java/org/talesof/talesofamysticland/controller/CharacterCreationController.java
@@ -1,14 +1,54 @@
 package org.talesof.talesofamysticland.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 public class CharacterCreationController {
+
+    @FXML
+    private VBox warriorBox, wizardBox, archerBox;
+
+    private VBox selectedBox;
     
-     @FXML 
+    @FXML 
     public void onClickImgOpenConfigurations() {
         // TODO
     }
 
+    @FXML
+    public void onClickSelectClass(MouseEvent event) {
+        setHoverEffect(warriorBox);
+        setHoverEffect(wizardBox);
+        setHoverEffect(archerBox);
+        
+        VBox clickedBox = (VBox) event.getSource();
+        setBorderStyle(clickedBox);
+    }
+
+    private void setBorderStyle(VBox box) {
+        if (selectedBox != null) {
+            selectedBox.setStyle("-fx-border-width: 0px");
+        }
+
+        box.setStyle("-fx-border-width: 3px");
+        selectedBox = box;
+    }
+
+    private void setHoverEffect(VBox box) {
+        box.setOnMouseEntered(e -> {
+            if (box != selectedBox) {
+                box.setStyle("-fx-border-width: 1px");
+            }
+        });
+
+        box.setOnMouseExited(e -> {
+            if (box != selectedBox) {
+                box.setStyle("-fx-border-width: 0px");
+            }
+        });
+    }
+    
     @FXML
     public void onActionBtnBack() {
         // TODO
